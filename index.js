16f5b9023961cb740daf2802ca715e7e8fec3fa0
pikarotattoo-ops <pikarotattoo@gmail.com>
2026-03-10 02:48:19 +0100

    Refactor string quotes and update request options

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
-const { inspect } = require("util");
-const yaml = require("js-yaml");
-
-const core = require("@actions/core");
-const { Octokit } = require("@octokit/action");
-
-main();
-
-async function main() {
-  const time = Date.now();
-
-  try {
-    const octokit = new Octokit();
-    const { route, ...parameters } = getAllInputs();
-
-    core.info(route);
-    for (const [name, value] of Object.entries(parameters)) {
-      core.info(`> ${name}: ${value}`);
-    }
-
-    // workaround for https://github.com/octokit/request-action/issues/71
-    // un-encode "repo" in /repos/{repo} URL when "repo" parameter is set to ${{ github.repository }}
-    const { url, body, ...options } = octokit.request.endpoint(
-      route,
-      parameters,
-    );
-    const requestOptions = {
-      ...options,
+      …options,
       data: body,
       url: url.replace(
         /\/repos\/([^/]+)/,
-        (_, match) => "/repos/" + decodeURIComponent(match),
+        (_, match) => “/repos/“ + decodeURIComponent(match),
       ),
     };
 
@@ -41,18 +14,18 @@ async function main() {
 
     core.info(`< ${status} ${Date.now() - time}ms`);
 
-    core.setOutput("status", status);
-    core.setOutput("headers", JSON.stringify(headers, null, 2));
+    core.setOutput(“status”, status);
+    core.setOutput(“headers”, JSON.stringify(headers, null, 2));
     core.setOutput(
-      "data",
-      typeof data === "object" ? JSON.stringify(data, null, 2) : data,
+      “data”,
+      typeof data === “object” ? JSON.stringify(data, null, 2) : data,
     );
   } catch (error) {
     if (error.status) {
       core.info(`< ${error.status} ${Date.now() - time}ms`);
     }
 
-    core.setOutput("status", error.status);
+    core.setOutput(“status”, error.status);
     core.debug(inspect(error));
     core.setFailed(error.message);
   }
@@ -63,9 +36,9 @@ function getAllInputs() {
     if (!/^INPUT_/.test(key)) return result;
 
     const inputName =
-      key.toLowerCase() === "input_mediatype"
-        ? "mediaType"
-        : key.substr("INPUT_".length).toLowerCase();
+      key.toLowerCase() === “input_mediatype”
+        ? “mediaType”
+        : key.substr(“INPUT_”.length).toLowerCase();
     result[inputName] = yaml.load(value);
 
     return result;
